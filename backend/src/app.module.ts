49d4b7e888284a981862a1132cc8dc572c39c4a1
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-09-14 17:12:05 +0200

    don't use db connection for now

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+//import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { GreetingService } from './app.service';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'postgres',
-      password: 'testpwd',
-      database: 'postgres',
-      //entities: [User],
-      synchronize: true
-    }),
-  ],
+ // imports: [
+ //   TypeOrmModule.forRoot({
+ //     type: 'postgres',
+ //     host: 'db',
+ //     port: 5432,
+ //     username: 'postgres',
+ //     password: 'testpwd',
+ //     database: 'postgres',
+ //     //entities: [User],
+ //     synchronize: true
+ //   }),
+ // ],
   controllers: [AppController],
   providers: [GreetingService],
 })
