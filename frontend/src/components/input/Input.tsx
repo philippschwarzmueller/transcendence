cdb0be7a465158559324cc77874d6a03a3e3a2c0
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-09-15 10:40:59 +0200

    clean up in component structure

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -1,3 +1,33 @@
-import { Input } from './style';
+import styled from 'styled-components';
+
+const Input = styled.input`
+  outline: none;
+  border: none;
+  cursor: text;
+
+  padding: 3 3 5 3;
+
+  color: rgb(0, 0, 0);
+  background-color: rgb(255, 255, 255);
+
+  border-radius: 0;
+  border-top-width: 1;
+  border-top-style: 1;
+  border-top-color: borderDark;
+
+  border-right-width: 0;
+  border-bottom-width: 0;
+
+  border-left-width: 1;
+  border-left-style: 1;
+  border-left-color: borderDark;
+
+  box-shadow:
+    inset -1px -1px 0px 0px rgb(195, 199, 203),
+    inset 1px 1px 0px 0px rgb(0, 0, 0),
+    0.5px 0.5px 0px 0.5px rgb(255, 255, 255);
+
+  -webkit-appearance: none;
+`;
 
 export default Input;
--- frontend/src/components/nav/Navbar.tsx
+++ frontend/src/components/nav/Navbar.tsx
@@ -1,20 +1,53 @@
 import React from 'react';
-import { StyledNavbar } from './style';
 import { Link } from 'react-router-dom';
+
+import styled from 'styled-components';
+
+const StyledNavbar = styled.nav`
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    -webkit-box-pack: justify;
+    width: 100vw;
+    z-index: 2;
+    background-color: rgb(195, 199, 203);
+    --x-shadow: inset 0.5px 0.5px 0px 0.5px #ffffff,
+                inset 0 0 0 1px #868a8e,
+                1px 0px 0 0px #000000,
+                0px 1px 0 0px #000000,
+                1px 1px 0 0px #000000;
+    box-shadow: var(--x-ring-shadow,0 0 #0000),var(--x-shadow);
+`;
+
+const StyledLink = styled(Link)`
+
+`;
+
+const StyledLi = styled.button`
+  background-color: rgb(195, 199, 203);
+  border: none;
+  outline: none;
+  max-width: 150px;
+  --x-shadow: inset 0.5px 0.5px 0px 0.5px #ffffff,
+              inset 0 0 0 1px #868a8e,
+              1px 9px 0 0px #000000,
+              0px 1px 0 0px #000000,
+              1px 1px 0 0px #000000
+`;
 
 const Navbar: React.FC = () => {
   return (
     <StyledNavbar>
         <ul style={{ listStyle: "none", display: "inline-flex" }}>
-          <li style={{ padding: 10 }}>
-            <Link to={"/home"}>Home</Link>
-          </li>
-          <li style={{ padding: 10 }}>
-            <Link to={"/react-basics"}>React basics</Link>
-          </li>
-          <li style={{ padding: 10 }}>
-            <Link to={"/congrats"}>Congrats</Link>
-          </li>
+          <StyledLi style={{ padding: 10 }}>
+            <StyledLink to={"/home"}>Home</StyledLink>
+          </StyledLi>
+          <StyledLi style={{ padding: 10 }}>
+            <StyledLink to={"/react-basics"}>React basics</StyledLink>
+          </StyledLi>
+          <StyledLi style={{ padding: 10 }}>
+            <StyledLink to={"/congrats"}>Congrats</StyledLink>
+          </StyledLi>
         </ul>
     </StyledNavbar>
   )
