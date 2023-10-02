1906560d7432cf3e28ab8010ca514808a7189d32
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-10-02 17:06:36 +0200

    remove create from user service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -10,14 +9,6 @@ export class UsersService {
     @InjectRepository(User)
     private usersRepository: Repository<User>,
   ) {}
-
-  create(user: CreateUserDto) {
-    if (!this.usersRepository.find()) {
-      this.usersRepository.save(user);
-    } else {
-      throw new Error('User exists');
-    }
-  }
 
   findAll(): Promise<User[]> {
     return this.usersRepository.find();
