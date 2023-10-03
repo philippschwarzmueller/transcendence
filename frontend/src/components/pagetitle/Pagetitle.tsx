d9c88f594b4d72058255477efc0ac7c6a21b06f1
Michael <116090305+migrae@users.noreply.github.com>
2023-10-03 09:42:15 +0200

    Mgraefen/ft 13 create signup page (#5)
    * basic signup route
    
    * added global styling
    
    * fix misc stuff like format and unused imports
    
    * rename Register component to SignUp
    
    * rename Register component to SignUp. rly this time
    
    ---------
    
    Co-authored-by: Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>

---
+++ frontend/src/components/pagetitle/Pagetitle.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Pagetitle = styled.h1`
+	background-color: #ffefd5;
+	color: #f4b1be;
+	border: none;
+	text-align: center;
+	font-size: 12;
+	font-family: 'Arial'
+`;
+
+export default Pagetitle;
