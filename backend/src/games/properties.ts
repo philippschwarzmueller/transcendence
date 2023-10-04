88598e4baae1fcfb618173f5732ed19b39c093a3
Florian Sandel <fsandel@student.42heilbronn.de>
2023-10-04 16:43:50 +0200

    Game: clean up

--- backend/src/games/properties.ts
+++ backend/src/games/properties.ts
@@ -11,16 +11,36 @@ interface IPaddle {
   color: string; //paddle color
 }
 
+interface IBallProperties {
+  radius: number; //radius of the painted ball
+  color: string; // color of the painted ball
+}
+
 interface IProperties {
   window: IWindow;
   paddle: IPaddle;
+  ballProperties: IBallProperties;
   framerate: number; // frontend game framerate
 }
 
+export interface IBall {
+  x: number;
+  y: number;
+  speed_x: number;
+  speed_y: number;
+}
+
 const properties: IProperties = {
-  window: { width: 960, height: 640, color: "black" },
-  paddle: { width: 2, height: 15, speed: 200, color: "white" },
+  window: { width: 960, height: 640, color: 'black' },
+  paddle: { width: 2, height: 15, speed: 200, color: 'white' },
+  ballProperties: { radius: 10, color: 'white' },
   framerate: 25,
 };
 
+export const ballSpawn: IBall = {
+  x: properties.window.width / 2,
+  y: properties.window.height / 2,
+  speed_x: 5,
+  speed_y: 5,
+};
 export default properties;
