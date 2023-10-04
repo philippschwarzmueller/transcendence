caba2569a003bbe26bb11cc4dfe89775906bc088
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-10-04 17:13:55 +0200

    add types, format code

--- backend/src/games/games.controller.ts
+++ backend/src/games/games.controller.ts
@@ -1,18 +1,19 @@
 import { Get, Controller, Post } from '@nestjs/common';
 import { GamesService } from './games.service';
+import { IBall } from './properties';
 
 @Controller('games')
 export class GamesController {
   constructor(private gamesService: GamesService) {}
 
   @Get('ball')
-  async ball(): Promise<any> {
+  async ball(): Promise<IBall> {
     return this.gamesService.ball();
   }
 
   @Post('start')
-  StartGameLoop(): any {
-    return this.gamesService.StartGameLoop();
+  startGameLoop(): number {
+    return this.gamesService.startGameLoop();
   }
 
   @Post('stop')
