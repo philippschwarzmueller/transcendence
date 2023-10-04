3bf7db9a423ff8167493259d27c6f00aaf4c610e
Michael Graefen <michaelgraefen@gmail.com>
2023-10-04 12:55:17 +0200

    basic routes created

---
+++ frontend/src/routes/Login.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Button from "../components/button";
+import Input from "../components/input";
+import Form from "../components/form";
+import Pagetitle from "../components/pagetitle";
+
+const Login: React.FC = () => {
+	return(
+		<Pagetitle>Login to your Account</Pagetitle>
+	);
+}
+
+export default Login;
