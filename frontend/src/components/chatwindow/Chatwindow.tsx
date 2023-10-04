f932bd180c263ed814b50b97f38223678cb59c88
oph-design <ole.paul.heinzelmann@gmail.com>
2023-10-04 15:47:42 +0200

    implement chatwindow component

--- frontend/src/components/chatwindow/Chatwindow.tsx
+++ frontend/src/components/chatwindow/Chatwindow.tsx
@@ -1,1 +1,28 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+// import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import React, { useState } from "react";
+import Input from "../input/Input";
+import Button from "../button/Button";
+
+interface IChatwindow {}
+
+const Chatwindow: React.FC<IChatwindow> = (props: IChatwindow) => {
+  let [messages, setMessages] = useState<string[]>([]);
+  let [input, setInput] = useState("");
+  return (
+    <>
+      <ul>
+        {messages.map((mes) => {
+          return <li>{mes}</li>;
+        })}
+      </ul>
+      <Input
+        label="Type here"
+        placeholder="Enter message"
+        onChange={(e) => setInput(e.target.value)}
+      ></Input>
+      <Button onClick={() => setMessages([...messages, input])}>Send</Button>
+    </>
+  );
+};
+
+export default Chatwindow;
--- frontend/src/routes/Chat.tsx
+++ frontend/src/routes/Chat.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Pagetitle from "../components/pagetitle/Pagetitle";
+import Chatwindow from "../components/chatwindow/Chatwindow";
 
 const Chat: React.FC = () => {
   return (
     <>
       <Pagetitle>Chat</Pagetitle>
+      <Chatwindow />
     </>
   );
 };
