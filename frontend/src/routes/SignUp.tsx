006789243b4313dcfd770957e1d77e10d7b82f83
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-10-05 13:01:21 +0200

    remove unused imports, add types, remove console.logs

--- frontend/src/routes/SignUp.tsx
+++ frontend/src/routes/SignUp.tsx
@@ -20,20 +20,15 @@ const SignUp: React.FC = () => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(input),
-        }
+        },
       );
-
       const data: any = await response.json();
-
-      console.log(data);
-
       if (response.ok) {
         alert(`User ${input.name} created succesfully`);
       } else {
         alert(data.message || "Signup failed");
       }
     } catch (error) {
-      console.error("An error occured", error);
       alert("An error occured. Please try again or ask pschwarz");
     }
   }
