3f0d60f86f1ba8b4f612736a248d738acddc87c1
florian <florian@sandel.email>
2023-10-06 20:21:12 +0200

    Game: now using RequestBody to transfer data instead of headers + general cleanup in backend

--- backend/src/games/games.service.ts
+++ backend/src/games/games.service.ts
@@ -1,24 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import properties, { ballSpawn, gameSpawn, IGame } from './properties';
+import properties, { gameSpawn, IGame, IPaddleBackend } from './properties';
 
-const newGame = (): IGame => {
+const newGameCopy = (): IGame => {
   return JSON.parse(JSON.stringify(gameSpawn));
 };
 
 @Injectable()
 export class GamesService {
   constructor() {
-    this.game = [newGame()];
+    this.games = [];
     this.intervals = [];
     this.amountOfGammes = 0;
   }
 
   public amountOfGammes: number;
-  public game: IGame[];
+  public games: IGame[];
   public intervals: NodeJS.Timeout[];
 
   stopAll(): void {
-    this.game = [newGame()];
+    this.games = [];
     this.amountOfGammes = 0;
     this.intervals.forEach((interval) => {
       clearInterval(interval);
@@ -28,41 +28,40 @@ export class GamesService {
 
   stop(gameId: number): void {
     clearInterval(this.intervals[gameId]);
+    this.games[gameId] = newGameCopy();
   }
 
-  async gamestate(
-    side: string,
-    height: number,
-    gameId: number,
-  ): Promise<IGame> {
-    if (this.amountOfGammes === 0) return newGame();
-    if (side === 'left')
-      this.game[gameId].left = { side: 'left', height: height };
-    if (side === 'right')
-      this.game[gameId].right = { side: 'right', height: height };
-    this.game[gameId].gameid = gameId;
-    return this.game[gameId];
+  async gamestate(paddle: IPaddleBackend, gameId: number): Promise<IGame> {
+    if (this.amountOfGammes === 0) return newGameCopy();
+    if (paddle.side === 'left')
+      this.games[gameId].left = { side: 'left', height: paddle.height };
+    if (paddle.side === 'right')
+      this.games[gameId].right = { side: 'right', height: paddle.height };
+    this.games[gameId].gameId = gameId;
+    return this.games[gameId];
   }
 
   public startGameLoop(): number {
-    this.game.push(newGame());
+    const newGame: IGame = newGameCopy();
     const gameId: number = this.amountOfGammes;
+    newGame.gameId = gameId;
+    this.games.push(newGame);
     this.amountOfGammes++;
     const interval = setInterval(() => {
       if (
-        this.game[gameId].ball.x + this.game[gameId].ball.speed_x >
+        this.games[gameId].ball.x + this.games[gameId].ball.speed_x >
           properties.window.width ||
-        this.game[gameId].ball.x + this.game[gameId].ball.speed_x < 0
+        this.games[gameId].ball.x + this.games[gameId].ball.speed_x < 0
       )
-        this.game[gameId].ball.speed_x *= -1;
+        this.games[gameId].ball.speed_x *= -1;
       if (
-        this.game[gameId].ball.y + this.game[gameId].ball.speed_y >
+        this.games[gameId].ball.y + this.games[gameId].ball.speed_y >
           properties.window.height ||
-        this.game[gameId].ball.y + this.game[gameId].ball.speed_y < 0
+        this.games[gameId].ball.y + this.games[gameId].ball.speed_y < 0
       )
-        this.game[gameId].ball.speed_y *= -1;
-      this.game[gameId].ball.x += this.game[gameId].ball.speed_x;
-      this.game[gameId].ball.y += this.game[gameId].ball.speed_y;
+        this.games[gameId].ball.speed_y *= -1;
+      this.games[gameId].ball.x += this.games[gameId].ball.speed_x;
+      this.games[gameId].ball.y += this.games[gameId].ball.speed_y;
     }, properties.framerate);
     this.intervals.push(interval);
     return gameId;
