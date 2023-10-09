b2d100e16d3db560837eeb6c26b7a907501040ba
LaurinUB <Laurinub@yahoo.de>
2023-10-09 18:51:32 +0200

    basic window

--- frontend/src/components/chatwindow/Chatwindow.tsx
+++ frontend/src/components/chatwindow/Chatwindow.tsx
@@ -1,7 +1,46 @@
+import styled from "styled-components"
 import React, { useState, useEffect } from "react";
 import Input from "../input/Input";
 import Button from "../button/Button";
 import { io } from "socket.io-client";
+
+const Msgfield = styled.div`
+  width: 320px;
+  padding: 10px;
+  border: none;
+  margin: 0;
+  background-color: rgb(195, 199, 203);
+  --x-shadow: inset 0.5px 0.5px 0px 0.5px #ffffff, inset 0 0 0 1px #868a8e,
+    1px 0px 0 0px #000000, 0px 1px 0 0px #000000, 1px 1px 0 0px #000000;
+  box-shadow: var(--x-ring-shadow, 0 0 #0000), var(--x-shadow);
+`;
+
+const Window = styled.div`
+`;
+
+const ChatNav = styled.div`
+  width: 320px;
+  padding: 10px;
+  background-color: rgb(195, 199, 203);
+  --x-shadow: inset 0.5px 0.5px 0px 0.5px #ffffff, inset 0 0 0 1px #868a8e,
+    1px 0px 0 0px #000000, 0px 1px 0 0px #000000, 1px 1px 0 0px #000000;
+  box-shadow: var(--x-ring-shadow, 0 0 #0000), var(--x-shadow);
+
+`;
+
+const Textfield = styled.div`
+  width: 334px;
+  height: 200px;
+  background-color: white;
+  border: 3px solid rgb(195, 199, 203);
+  --x-shadow: inset 0.5px 0.5px 0px 0.5px #ffffff, inset 0 0 0 1px #868a8e,
+    1px 0px 0 0px #000000, 0px 1px 0 0px #000000, 1px 1px 0 0px #000000;
+  box-shadow: var(--x-ring-shadow, 0 0 #0000), var(--x-shadow);
+`;
+
+const StyledUl = styled.ul`
+  list-style: none;
+`;
 
 const Chatwindow: React.FC = () => {
   let [messages, setMessages] = useState<string[]>([]);
@@ -17,6 +56,9 @@ const Chatwindow: React.FC = () => {
     });
   }, [socket]);
 
+  function move(event: React.MouseEvent): void {
+    }
+
   function send(event: React.MouseEvent): void {
     event.preventDefault();
     if (input.trim() !== "") {
@@ -29,22 +71,29 @@ const Chatwindow: React.FC = () => {
 
   return (
     <>
-      <ul>
-        {messages.map((mes) => {
-          return <li key={mes}>{mes}</li>;
-        })}
-      </ul>
-      <Input
-        value={input}
-        label="Type here"
-        placeholder="Enter message"
-        onChange={(e) => setInput(e.target.value)}
-      ></Input>
-      <Button onClick={(event: React.MouseEvent) => send(event)}>Send</Button>
-      <Button onClick={() => setSocket(io("ws://localhost:8080"))}>
+    <Window>
+    <ChatNav onClick={(event: React.MouseEvent) => move(event)}>ClickHere</ChatNav>
+      <Textfield>
+        <StyledUl>
+          {messages.map((mes) => {
+            return <li key={mes}>{mes}</li>;
+          })}
+        </StyledUl>
+      </Textfield>
+      <Msgfield>
+        <Input
+          value={input}
+          label="Type here"
+          placeholder="Enter message"
+          onChange={(e) => setInput(e.target.value)}
+        ></Input>
+        <Button onClick={(event: React.MouseEvent) => send(event)}>Send</Button>
+        <Button onClick={() => setSocket(io("ws://localhost:8080"))}>
         Connect
-      </Button>
-      <Button onClick={() => socket.disconnect()}>Disconnect</Button>
+        </Button>
+        <Button onClick={() => socket.disconnect()}>Disconnect</Button>
+      </Msgfield>
+    </Window>
     </>
   );
 };
