d7673785abfb4515e0f23f0facbd85ba0549fb15
Florian Sandel <fsandel@student.42heilbronn.de>
2023-10-09 13:24:45 +0200

    removal of unsued comment in certerdiv

--- frontend/src/components/centerdiv/Centerdiv.tsx
+++ frontend/src/components/centerdiv/Centerdiv.tsx
@@ -1,12 +1,5 @@
 import styled from "styled-components";
 
-// const Centerdiv = styled.div`
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   width: 100%;
-// `;
-
 const Centerdiv = styled.div`
   display: flex;
   justify-content: center;
