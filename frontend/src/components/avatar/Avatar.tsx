c584f8206602dd9972c7f0ce6165a0c92c3b5d94
Michael Graefen <michaelgraefen@gmail.com>
2023-10-16 10:41:25 +0200

    form

--- frontend/src/components/avatar/Avatar.tsx
+++ frontend/src/components/avatar/Avatar.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const ImageContainer = styled.div`
-	width: 50px;
-	height: 50px;
-	border-radius: 50%;
-	overflow: hidden;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  overflow: hidden;
 `;
 
 const StyledImg = styled.img`
   width: 100%;
   height: 100%;
-	object-fit: cover;
-	object-position: left-top;
+  object-fit: cover;
+  object-position: left-top;
 `;
 
 export interface IAvatar {
@@ -45,9 +45,9 @@ const Avatar: React.FC<IAvatar> = ({ name = "user" }) => {
 
   return (
     <>
-		<ImageContainer>
-      <StyledImg src={src} alt={`${name}'s avatar`} />
-		</ImageContainer>
+      <ImageContainer>
+        <StyledImg src={src} alt={`${name}'s avatar`} />
+      </ImageContainer>
     </>
   );
 };
