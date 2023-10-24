6fddef48fc1ced718f33aab5eb5585bbdee40f5b
oph-design <115570424+oph-design@users.noreply.github.com>
2023-10-24 16:31:23 +0200

    saved mgraefens pr

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,6 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);0
   app.enableCors({
-    origin: 'http://localhost:3000',
     credentials: true,
   });
 	app.use(cookieParser());
