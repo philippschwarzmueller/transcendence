ecdf4aad0c8e885e1642b39711b807dd7103b530
Florian Sandel <fsandel@student.42heilbronn.de>
2023-10-30 13:25:58 +0100

    Game-Database-Bug: fixed bug where checking for undefined winner isntead of undefined looser and filled empty database entries

--- backend/src/games/games.service.ts
+++ backend/src/games/games.service.ts
@@ -221,8 +221,22 @@ export class GamesService {
     if (!databaseGame) return;
     const updatedDatabaseGame: CreateGameDto = {
       gameId: localGame.gameId,
-      winner: winner != null && winner != undefined ? winner.name : 'null',
-      looser: looser != null && winner != undefined ? looser.name : 'null',
+      winner:
+        winner != null &&
+        winner != undefined &&
+        winner.name != '' &&
+        winner.name != undefined &&
+        winner.name != null
+          ? winner.name
+          : 'null',
+      looser:
+        looser != null &&
+        looser != undefined &&
+        looser.name != '' &&
+        looser.name != undefined &&
+        looser.name != null
+          ? looser.name
+          : 'null',
       winnerPoints: winnerPoints,
       looserPoints: looserPoints,
       isFinished: true,
