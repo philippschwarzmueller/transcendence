b9b8a396247c5030a2bd5883613ed8fcd358d755
Florian Sandel <fsandel@student.42heilbronn.de>
2023-11-08 11:18:12 +0100

    Game: remove canvas border because it looked ass

--- frontend/src/components/gamecanvas/Gamecanvas.tsx
+++ frontend/src/components/gamecanvas/Gamecanvas.tsx
@@ -2,11 +2,18 @@ import styled from "styled-components";
 
 const Gamecanvas = styled.canvas`
   background-color: rgba(0, 0, 0, 0);
-  border: 3px solid #000000;
+  /* border: 3px solid #000000; */
   position: absolute;
   transform: translate(-50%, 0);
   width: ${(props) => props.width || "100%"};
   height: ${(props) => props.height || "100%"};
+  outline: none; /* Add outline property to remove the outline */
+  -webkit-tap-highlight-color: rgba(
+    255,
+    255,
+    255,
+    0
+  ); /* Add -webkit-tap-highlight-color property for mobile webkit */
 `;
 
 export default Gamecanvas;
