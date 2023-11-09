fba0cfa062c369a656167b80f6186fab7e6e37fb
Michael Graefen <michaelgraefen@gmail.com>
2023-11-09 15:30:09 +0100

    protection working with re-auth

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
-import { Navigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
+import { Navigate } from 'react-router-dom';
 
 interface PrivateRouteProps {
   children: JSX.Element;
@@ -8,11 +9,47 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [isValid, setValidity] = useState<boolean | null>(null);
 
-  if (auth.user.name === undefined) {
+  useEffect(() => {
+    const validateToken = async () => {
+      try {
+        const response = await fetch("http://localhost:4000/auth/validate-token", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+        });
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+				console.log(data);
+        if (data) {
+          auth.logIn(data.name);
+          setValidity(true);
+        } else {
+          setValidity(false);
+        }
+      } catch (error) {
+        console.error("There has been a problem with your fetch operation:", error);
+        setValidity(false);
+      }
+    };
+    validateToken();
+  }, [auth]);
+
+  if (isValid === null) {
+    return <div>Loading...</div>;
+  }
+  if (!isValid) {
     return <Navigate to="/login" replace />;
   }
-
   return children;
 };
 
