942ca4b76b72d2a7fb67d6bcff6cb35c326e1575
Michael Graefen <michaelgraefen@gmail.com>
2023-11-16 12:31:03 +0100

    no more localhost

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,14 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);0
+  const app = await NestFactory.create(AppModule);
+  const configService = new ConfigService();
+  const hostIP = configService.get<string>('HOST_IP');
+  console.log(hostIP);
   app.enableCors({
-		origin: ['http://localhost:3000', 'http://localhost:8080'],
+    origin: [`http://${hostIP}:3000`, `http://${hostIP}:9000`],
     credentials: true,
   });
-	app.use(cookieParser());
+  app.use(cookieParser());
   await app.listen(4000);
 }
 bootstrap();
