2ba14e8b6a7bc1883addd6fa2e1f94bad551dab7
florian <florian@sandel.email>
2023-11-17 17:42:38 +0100

    Game: add controller to get won/lost games

---
+++ backend/src/games/games.controller.ts
@@ -0,0 +1,97 @@
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { User } from '../users/user.entity';
+import { Game } from './game.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
+interface IMatch {
+  winnerNickname: string;
+  looserNickname: string;
+  winnerPoints: number;
+  looserPoints: number;
+  wonGame: boolean;
+}
+
+@Controller('games')
+export class GamesController {
+  constructor(
+    @InjectRepository(Game)
+    private gamesRepository: Repository<Game>, // private configService: ConfigService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  @Get('getwongamesamount/:intraname')
+  public async getWonGamesAmount(
+    @Param('intraname') intraname: string,
+  ): Promise<number> {
+    const user: User = await this.userRepository.findOne({
+      where: { intraname: intraname },
+      relations: ['wonGames'],
+    });
+    if (!user) throw new BadRequestException('user not found');
+    return user.wonGames.length;
+  }
+
+  @Get('getlostgamesamount/:intraname')
+  public async getLostGamesAmount(
+    @Param('intraname') intraname: string,
+  ): Promise<number> {
+    const user: User = await this.userRepository.findOne({
+      where: { intraname: intraname },
+      relations: ['lostGames'],
+    });
+    if (!user) throw new BadRequestException('user not found');
+    return user.lostGames.length;
+  }
+
+  @Get('gettotalgamesamount/:intraname')
+  public async getTotalGamesAmount(
+    @Param('intraname') intraname: string,
+  ): Promise<number> {
+    return (
+      (await this.getWonGamesAmount(intraname)) +
+      (await this.getLostGamesAmount(intraname))
+    );
+  }
+
+  @Get('getallgames/:intraname')
+  public async getallgames(
+    @Param('intraname') intraname: string,
+  ): Promise<IMatch[]> {
+    const user: User = await this.userRepository.findOne({
+      where: { intraname: intraname },
+      relations: [
+        'wonGames',
+        'wonGames.winner',
+        'wonGames.looser',
+        'lostGames',
+        'lostGames.winner',
+        'lostGames.looser',
+      ],
+    });
+    if (!user) throw new BadRequestException('user not found');
+    const Matches: IMatch[] = [];
+    user.wonGames.forEach((game) => {
+      if (!game.isFinished) return;
+      Matches.push({
+        winnerNickname: game.winner.intraname,
+        looserNickname: game.looser.intraname,
+        winnerPoints: game.winnerPoints,
+        looserPoints: game.looserPoints,
+        wonGame: game.winner.name === intraname,
+      });
+    });
+    user.lostGames.forEach((game) => {
+      if (!game.isFinished) return;
+      Matches.push({
+        winnerNickname: game.winner.intraname,
+        looserNickname: game.looser.intraname,
+        winnerPoints: game.winnerPoints,
+        looserPoints: game.looserPoints,
+        wonGame: game.winner.name === intraname,
+      });
+    });
+    return Matches;
+  }
+}
--- backend/src/games/games.module.ts
+++ backend/src/games/games.module.ts
@@ -3,9 +3,11 @@ import { GamesService } from './games.service';
 import { Game } from './game.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
+import { GamesController } from './games.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Game, User])],
+  controllers: [GamesController],
   providers: [GamesService],
   exports: [GamesService],
 })
