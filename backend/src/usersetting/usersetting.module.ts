51d600d912e007c8c6b410c5a2951f7a69e92d5a
Michael Graefen <michaelgraefen@gmail.com>
2023-11-21 14:30:58 +0100

    rm usersetting

--- backend/src/usersetting/usersetting.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UsersettingService } from './usersetting.service';
-import { UsersettingController } from './usersetting.controller';
-
-@Module({
-  controllers: [UsersettingController],
-  providers: [UsersettingService],
-})
-export class UsersettingModule {}
