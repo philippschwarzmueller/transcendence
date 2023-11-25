993eb60717b5852d662dad24989041606a52787e
LaurinUB <Laurinub@yahoo.de>
2023-11-25 18:45:52 +0100

    adding friend/user browser

--- frontend/src/routes/Game.tsx
+++ frontend/src/routes/Game.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Pagetitle from "../components/pagetitle/Pagetitle";
 
 import GameWindow from "../components/gamewindow/";
+import Taskbar from "../components/taskbar/Taskbar";
 
 const Game: React.FC = () => {
   return (
     <>
       <Pagetitle>Game</Pagetitle>
       <GameWindow></GameWindow>
+      <Taskbar />
     </>
   );
 };
