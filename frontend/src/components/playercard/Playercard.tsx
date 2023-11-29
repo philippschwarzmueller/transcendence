e0c76acab3a1d799dfecac68f54a02c03b89ff4c
LaurinUB <Laurinub@yahoo.de>
2023-11-29 16:01:27 +0100

    basic set

--- frontend/src/components/playercard/Playercard.tsx
+++ frontend/src/components/playercard/Playercard.tsx
@@ -15,20 +15,9 @@ const StyledDiv = styled.div`
     rgb(134, 138, 142) 0px 0px 0px 1px inset, rgb(0, 0, 0) 1px 1px 0px 1px;
   padding: 8px;
   cursor: pointer;
-  &:focus {
-    outline: 1px dotted rgb(0, 0, 0);
-    outline-offset: -5px;
-
-    box-shadow: inset 1px 1px 0px 1px rgb(255, 255, 255),
-      inset 0 0 0 1px rgb(134, 138, 142), 1px 1px 0 0px rgb(0, 0, 0);
-  }
-
   &:active {
-    padding: 8 20 4;
-
     outline: 1px dotted rgb(0, 0, 0);
     outline-offset: -5px;
-
     box-shadow: inset 0 0 0 1px rgb(134, 138, 142), 0 0 0 1px rgb(0, 0, 0);
   }
 `;
@@ -43,13 +32,21 @@ const PlayerCard: React.FC<PlayerCardProps> = ({ user, triggerReload }) => {
   let [y, setY] = useState<number>(0);
 
   function openContextMenu(e: React.MouseEvent) {
-    setX(e.pageX);
-    setY(e.pageY);
+    setX(e.pageX - e.clientX);
+    setY(e.pageY - e.clientY);
     setShowContext(!showContext);
   }
 
   return (
     <>
+      <ContextMenu
+        display={showContext}
+        positionX={x}
+        positionY={y}
+        user={user}
+        triggerReload={triggerReload}
+      />
+      <div onClick={(e) => openContextMenu(e)}>
       <StyledDiv onClick={(e) => openContextMenu(e)}>
         <Avatar
           name={user.name}
@@ -62,13 +59,7 @@ const PlayerCard: React.FC<PlayerCardProps> = ({ user, triggerReload }) => {
           <p style={{ margin: "3px" }}>W/L%: 40</p>
         </div>
       </StyledDiv>
-      <ContextMenu
-        display={showContext}
-        positionX={x}
-        positionY={y}
-        user={user}
-        triggerReload={triggerReload}
-      />
+      </div>
     </>
   );
 };
