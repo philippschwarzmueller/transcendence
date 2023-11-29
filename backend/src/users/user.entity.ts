d7d80f2dc70eead8922108f167934d9b59f961a8
Michael Graefen <michaelgraefen@gmail.com>
2023-11-29 12:57:08 +0100

    profilepicture component fetches user

--- backend/src/users/user.entity.ts
+++ backend/src/users/user.entity.ts
@@ -122,7 +122,7 @@ export class User {
   elo: number[];
 
   @Column('text', { nullable: true })
-  upladedAvatar: string;
+  customAvatar: string;
 
   @Column({ default: false })
   hasCustomAvatar: boolean;
