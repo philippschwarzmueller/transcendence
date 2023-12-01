b1753f83c86ef8e4aa26ad1479efec16dbc7160d
LaurinUB <Laurinub@yahoo.de>
2023-12-01 15:46:03 +0100

    get propper winrate

--- frontend/src/components/profilewindow/Profilewindow.tsx
+++ frontend/src/components/profilewindow/Profilewindow.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { styled } from "styled-components";
 import { IProfileContext, ProfileContext } from "../../context/profile";
 import Elograph from "../elograph";
