f0ea281b1e593c2a3dae861baa7f34a9b814245e
LaurinUB <Laurinub@yahoo.de>
2023-12-02 16:32:33 +0100

    styling

--- frontend/src/components/channeluser/ChannelUser.tsx
+++ frontend/src/components/channeluser/ChannelUser.tsx
@@ -32,12 +32,46 @@ const StyledDiv = styled.div<{
     rgb(0, 0, 0) 1px 1px 0px 1px;
 `;
 
+const Windowbar = styled.div`
+  height: 18px;
+  margin-bottom: 2px;
+  padding: 2px;
+  display: flex;
+  gap: 10px;
+  box-shadow: none;
+  background: rgb(0, 14, 122);
+  color: White;
+  font-size: 1em;
+  cursor: grab;
+`;
+
 const StyledUl = styled.ul`
   border: none;
   margin: 0px;
   padding: 2px;
   list-style: none;
   width: auto;
+  max-height: 200px;
+  overflow: auto;
+  &::-webkit-scrollbar {
+    width: 17x;
+    height: 17px;
+  }
+  &::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+  }
+  &::-webkit-scrollbar-thumb {
+    box-sizing: border-box;
+    display: inline-block;
+    background: rgb(195, 199, 203);
+    color: rgb(0, 0, 0);
+    border: 0px;
+    box-shadow:
+      rgb(0, 0, 0) -1px -1px 0px 0px inset,
+      rgb(210, 210, 210) 1px 1px 0px 0px inset,
+      rgb(134, 138, 142) -2px -2px 0px 0px inset,
+      rgb(255, 255, 255) 2px 2px 0px 0px inset;
+  }
 `;
 
 interface props {
@@ -66,7 +100,7 @@ function ChannelUser({ id }: props, ref: Ref<refs>) {
   }
 
   useEffect(() => {
-    socket.emit('getChannelUser', id, (res: IUser[]) => setUsers(res));
+    socket.emit("getChannelUser", id, (res: IUser[]) => setUsers(res));
   }, [display]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
@@ -77,7 +111,7 @@ function ChannelUser({ id }: props, ref: Ref<refs>) {
         $posY={positionY}
         onMouseLeave={() => setDisplay(!display)}
       >
-        {"Channel users"}
+        <Windowbar>{"Channel users"}</Windowbar>
         <StyledUl>
           {users.map((user) => {
             return <Playercard user={user} key={user.name} />;
