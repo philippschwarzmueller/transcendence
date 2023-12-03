9cd33108effec3597cd879f2456f4f49a5c42153
LaurinUB <Laurinub@yahoo.de>
2023-12-03 14:45:11 +0100

    remove credentials, remove strict mode

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,9 +5,7 @@ import "./index.css";
 import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 );
